5857414861a90ec1adf111b1e5793b858df744c7
RavenKMusic <85319728+ravenkmusic@users.noreply.github.com>
2022-01-07 17:53:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+s = 15
+m = 20
+l = 25
+
+if size == 15:
+  print("A small pizza is $15.")
+if size == 20:
+  print("A medium pizza is $20.")
+if size == 25:
+  print("A large pizza is $25.")
+else:
+  print("We don't have that.")
